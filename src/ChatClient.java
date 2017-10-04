d79714cef35455bcc7b892a644a1131c4d27d167
Adrian Cuellar <aacuellar14@gmail.com>
2017-10-04 16:06:53 -0700

    basicImp

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,35 +1,46 @@
 
-import java.io.InputStream;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 
 public final class ChatClient {
 
     public static void main(String[] args) throws Exception {
-        try (Socket socket = new Socket("localhost", 22222)) {
+
+        try (Socket socket = new Socket("18.221.102.182",38001)) {
             InputStream is = socket.getInputStream();
             InputStreamReader isr = new InputStreamReader(is, "UTF-8");
             BufferedReader br = new BufferedReader(isr);
-            System.out.println(br.readLine());
             BufferedReader brIS = new BufferedReader(new InputStreamReader(System.in));
             PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
-            System.out.print("Please enter your username:");
+
+            System.out.println("Please enter your username:");
             String inp;
-            String name = br.readLine().trim();
             String sMessage;
+            out.println(brIS.readLine());
+            sMessage=br.readLine();
+            if(sMessage.equals("Name in use.")) {
+                return;
+            }
+
+            Runnable listen = () ->{
+                String sMess;
+                while(true){
+                    try{
+                        sMess=br.readLine();
+                        System.out.println(sMess);
+                    }
+                    catch (IOException e){
+
+                    }
+                }
+            };
+
+            listen.run();
             while((inp=brIS.readLine())!=null){
                 out.println(inp);
-                sMessage=br.readLine();
-                if(sMessage.equals("Name in use.")) {
-                    break;
-                }
-                else{
-                    System.out.println(sMessage);
-                }
                 System.out.print("Your message:");
             }
+
             is.close();
             isr.close();
             br.close();
